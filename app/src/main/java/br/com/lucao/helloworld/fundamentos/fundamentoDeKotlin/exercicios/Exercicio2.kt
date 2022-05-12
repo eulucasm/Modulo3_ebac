9dcc68207d3ad6accbf3c12b8d3d01102f43cbe9
eulucasm <lucaspoletis@gmail.com>
2022-05-12 09:55:22 -0300

    organização

--- app/src/main/java/br/com/lucao/helloworld/fundamentos/fundamentoDeKotlin/exercicios/Exercicio2.kt
+++ app/src/main/java/br/com/lucao/helloworld/fundamentos/fundamentoDeKotlin/exercicios/Exercicio2.kt
@@ -6,5 +6,5 @@ fun main() {
 //   val valor = 6
 //   valor = 7
 //   println(valor)
-   //não foi possivel alterar o valor pois um val é imutavel, ou seja, impossivel de se alterar o valor
+//   não foi possivel alterar o valor pois um val é imutavel, ou seja, impossivel de se alterar o valor
 }
